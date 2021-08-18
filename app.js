4ef573f77a0a104d5761ceef427ff4006bdd3669
jayeen29 <jayeen6783@gmail.com>
2021-08-19 02:21:32 +0600

    function added for keypad typing and inputing value

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ function getPin() {
 function generatePin() {
     document.getElementById('generate-pin-input').value = getPin();
 }
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const keyTyped = event.target.innerText;
+    const keyInput = document.getElementById('key-input');
+    if (isNaN(keyTyped)) {
+        if (keyTyped == 'C') {
+            keyInput.value = '';
+        }
+    }
+    else {
+        const previousKeyInput = keyInput.value;
+        const newKeyInput = previousKeyInput + keyTyped;
+        if (newKeyInput.length > 4) {
+            return;
+        }
+    }
+});
